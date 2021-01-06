af93befa899c8293be1c98c3b78e4401942d5693
Yuki Sato <yuki.sato@lightcurve.io>
2021-01-06 20:39:57 +0100

    TMS: add controller to post data

--- tms/controller.js
+++ tms/controller.js
@@ -1,9 +1,16 @@
+const fs = require('fs');
+
 const getTranslation = async (req, res) => {
   res.send('translated');
 };
 
-const postData = async (req, res) => {
-  res.send('data posted');
+const postData = async (req, res, next) => {
+  const data = JSON.stringify(req.body)
+
+  fs.writeFile('./data.json', data, (err) => {
+    if (err) return console.log(err)
+  })
+  res.json('data posted');
 };
 
 module.exports = {
