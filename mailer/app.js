18f71b5dfb52a91f7f7cdafcbd793c5b41456770
Yuki Sato <yuki.sato@lightcurve.io>
2021-01-07 14:35:24 +0100

    Remove comment from mailer app.js

--- mailer/app.js
+++ mailer/app.js
@@ -1,21 +1,82 @@
+require('dotenv').config()
 const nodemailer = require('nodemailer');
+
+const amqp = require('amqplib');
+const messageQueueConnectionString = process.env.CLOUDAMQP_URL;
+
+async function listenForMessages() {
+  let connection = await amqp.connect(messageQueueConnectionString);
+  let channel = await connection.createChannel();
+  await channel.prefetch(1);
+  let resultsChannel = await connection.createConfirmChannel();
+  await consume({ connection, channel, resultsChannel });
+}
+
+function publishToChannel(channel, { routingKey, exchangeName, data }) {
+  return new Promise((resolve, reject) => {
+    channel.publish(exchangeName, routingKey, Buffer.from(JSON.stringify(data), 'utf-8'), { persistent: true }, function (err, ok) {
+      if (err) {
+        return reject(err);
+      }
+      resolve();
+    })
+  });
+}
+
+// consume messages from RabbitMQ
+function consume({ connection, channel, resultsChannel }) {
+  return new Promise((resolve, reject) => {
+    channel.consume("processing.requests", async function (msg) {
+      let msgBody = msg.content.toString();
+      let data = JSON.parse(msgBody);
+      let requestId = data.requestId;
+      let requestData = data.requestData;
+      console.log('data:', data)
+      console.log("Received a request message, requestId:", requestId);
+      let processingResults = await processMessage(requestData);
+
+      await publishToChannel(resultsChannel, {
+        exchangeName: "processing",
+        routingKey: "result",
+        data: { requestId, processingResults }
+      });
+      console.log("Published results for requestId:", requestId);
+
+      await channel.ack(msg);
+    });
+
+    connection.on("close", (err) => {
+      return reject(err);
+    });
+
+    connection.on("error", (err) => {
+      return reject(err);
+    });
+  });
+}
+
+// simulate data processing that takes 5 seconds
+function processMessage(requestData) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(requestData + "-processed")
+    }, 5000);
+  });
+}
+
+listenForMessages();
+
 // async..await is not allowed in global scope, must use a wrapper
 async function main() {
-  // Generate test SMTP service account from ethereal.email
-  // Only needed if you don't have a real mail account for testing
-  let testAccount = await nodemailer.createTestAccount();
-
-  // create reusable transporter object using the default SMTP transport
   let transporter = nodemailer.createTransport({
-    host: "smtp.mailtrap.io",
-    port: 2525,
+    host: process.env.MAILTRAP_HOST || "smtp.mailtrap.io",
+    port: process.env.MAILTRAP_PORT || 2525,
     auth: {
-      user: "a61aaa72bda965",
-      pass: "00150bbfde5713"
+      user: process.env.MAILTRAP_USER,
+      pass: process.env.MAILTRAP_PASS
     }
   });
 
-  // send mail with defined transport object
   let info = await transporter.sendMail({
     from: '"Yuki Sato 👻" <sato.youxi@gmail.com>', // sender address
     to: "e9ec4dc18b-5af4c5@inbox.mailtrap.io", // list of receivers
@@ -25,11 +86,7 @@ async function main() {
   });
 
   console.log("Message sent: %s", info.messageId);
-  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-
-  // Preview only available when sending through an Ethereal account
   console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
 }
 
-main().catch(console.error);
+// main().catch(console.error);
