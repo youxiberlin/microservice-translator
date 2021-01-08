3db585c4dd27a4db5fea76926dbf3d5afb1f8650
Yuki Sato <yuki.sato@lightcurve.io>
2021-01-08 22:25:53 +0100

    Remove unnecessary dependencies

--- translator/routes.js
+++ translator/routes.js
@@ -1,21 +1,7 @@
 const express = require('express');
-const path = require('path');
-const multer = require('multer');
 const { postText } = require('./controller');
 const router = express.Router();
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-      cb(null, 'data/uploads');
-  },
-  filename: (req, file, cb) => {
-      cb(null, Date.now() + path.extname(file.originalname));
-  }
-});
-
-const upload = multer({ storage });
-
 router.post('/', postText);
-// router.post('/', upload.single('subtitle-text'), postText);
 
 module.exports = router;
