9e4211090a5ff2b8353e15951905ba0e7fec5c90
Yuki Sato <yuki.sato@lightcurve.io>
2021-01-08 20:50:58 +0100

    Add attachment to mailer

--- mailer/controller.js
+++ mailer/controller.js
@@ -1,17 +1,28 @@
-const fs =  require('fs');
+const fs = require('fs').promises;
 const { mailer } = require('./lib/mailer');
 
 const postText = async (req, res, next) => {
-  console.log('req body', req.body)
-  const data = req.body.data;
+  const translationResults = req.body.data;
+  const filename = `${req.body.docId}.txt`;
+  const path = `data/email/${filename}`;
+  try {
+    await fs.appendFile(path, translationResults)
+  } catch(err) {
+    console.error(err)
+  }
 
-  // TODO save the e-mail data locally as text file
   await mailer(
     {
       from: '"Yuki Sato 👻" <sato.youxi@gmail.com>', // sender address
       to: "e9ec4dc18b-5af4c5@inbox.mailtrap.io", // list of receivers
       subject: "Here is your translation ✔", // Subject line
-      text: data,
+      text: translationResults,
+      attachments: [
+        {
+          filename: 'translation_result.txt',
+          path,
+        }
+      ]
     }
   ).catch(console.err);
 
